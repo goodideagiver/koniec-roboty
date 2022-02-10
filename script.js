e44e662ca75431f610574a9b7de06a593e51d6e5
Karol <kariurek@gmail.com>
2022-02-10 12:22:34 +0100

    customise working hour

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const counterElement = document.getElementById('end-counter');
 const appMain = document.getElementById('app');
+const header = appMain.querySelector('h1');
 appMain.style.transition = '.2s';
+
+let WORK_START_HR = parseInt(prompt('Godzina o której rozpoczynasz robotę', 8));
+let WORK_END_HR = parseInt(prompt('Godzina o której kończysz robotę', 16));
 
 const setStyle = scale => {
 	appMain.style.transform = 'scale(' + scale + ')';
@@ -8,17 +12,18 @@ const setStyle = scale => {
 
 const timeHandler = localeTime => {
 	const [hour, minute, second] = localeTime.split(':');
-	if (hour < 16 && hour >= 8) {
-		counterElement.innerText = `${16 - hour} godz ${
+	if (hour < WORK_END_HR && hour >= WORK_START_HR) {
+		counterElement.innerText = `${WORK_END_HR - hour} godz ${
 			59 - minute < 10 ? '0' + (59 - minute) : 59 - minute
 		} min ${59 - second < 10 ? '0' + (59 - second) : 59 - second} sek`;
 		setStyle(1 + second / 70);
-	} else if (hour > 16) {
-		counterElement.inputMode = 'Po robocie';
+	} else if (hour > WORK_END_HR) {
+		header.innerText = 'Robota zakończona';
+		counterElement.innerText = '';
 	} else {
-		counterElement.innerText = `Robota za ${8 - hour} godz ${59 - minute} min ${
-			59 - second
-		} sek`;
+		counterElement.innerText = `Robota za ${WORK_START_HR - hour} godz ${
+			59 - minute
+		} min ${59 - second} sek`;
 	}
 };
 
