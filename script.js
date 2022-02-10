c39aaa12a8f0aa13543af4091b5eec479c91b833
Karol <kariurek@gmail.com>
2022-02-10 12:35:26 +0100

    added progress

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const counterElement = document.getElementById('end-counter');
 const appMain = document.getElementById('app');
 const header = appMain.querySelector('h1');
+const progress = document.getElementById('progress');
 appMain.style.transition = '.2s';
 
 let WORK_START_HR = parseInt(prompt('Godzina o której rozpoczynasz robotę', 8));
 let WORK_END_HR = parseInt(prompt('Godzina o której kończysz robotę', 16));
+
+progress.setAttribute('max', WORK_END_HR - WORK_START_HR);
 
 const setStyle = scale => {
 	appMain.style.transform = 'scale(' + scale + ')';
@@ -17,6 +20,7 @@ const timeHandler = localeTime => {
 			59 - minute < 10 ? '0' + (59 - minute) : 59 - minute
 		} min ${59 - second < 10 ? '0' + (59 - second) : 59 - second} sek`;
 		setStyle(1 + second / 70);
+		progress.value = hour - WORK_START_HR;
 	} else if (hour > WORK_END_HR) {
 		header.innerText = 'Robota zakończona';
 		counterElement.innerText = '';
