7b11f1854b2419f1c89a11f5815a08855ad4f8b2
Karol <kariurek@gmail.com>
2022-02-10 11:48:51 +0100

    simple style

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const counterElement = document.getElementById('end-counter');
+const appMain = document.getElementById('app');
+appMain.style.transition = '.2s';
+
+const setStyle = scale => {
+	appMain.style.transform = 'scale(' + scale + ')';
+};
 
 const timeHandler = localeTime => {
 	const [hour, minute, second] = localeTime.split(':');
@@ -6,6 +12,7 @@ const timeHandler = localeTime => {
 		counterElement.innerText = `${16 - hour} godz ${
 			59 - minute < 10 ? '0' + (59 - minute) : 59 - minute
 		} min ${59 - second < 10 ? '0' + (59 - second) : 59 - second} sek`;
+		setStyle(1 + second / 70);
 	} else if (hour > 16) {
 		counterElement.inputMode = 'Po robocie';
 	} else {
